466fdbab1f2bbe35d76728d56b2285699fadb331
david <devish.c@gmail.com>
2017-05-09 08:23:05 -0500

    linkdef update --DC

--- LinearityStudies/LinkDef.h
+++ LinearityStudies/LinkDef.h
@@ -10,6 +10,8 @@
 #pragma link off all functions;
 
 #pragma link C++ class larlite::ClusterLinearityStudy+;
+#pragma link C++ class larlite::CalculateHitRemovalEff+;
 //ADD_NEW_CLASS ... do not change this line
 #endif
+
 
