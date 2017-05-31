bfcbc860a83ee22aa8cb7de70dd39e5c8640243a
david <devish.c@gmail.com>
2017-05-31 16:26:49 -0500

    removing unused module --DC

--- CosmicRemoval/LinkDef.h
+++ CosmicRemoval/LinkDef.h
@@ -9,7 +9,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class larlite::CosmicRemoval+;
 #pragma link C++ class larlite::RemoveCosmicTracks+;
 #pragma link C++ class larlite::RemoveDeltaRays+;
 #pragma link C++ class larlite::ROIRemoval+;
